c5bf6b3109a677a767f689da0f762049578f1ebd
wodzu96 <p.wodzu96@gmail.com>
2016-03-31 01:10:37 +0200

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -1,46 +1,112 @@
-// Interfejs
-class Animal
+#include <iostream>
+#include <cstdio>
+using namespace std;
+class Vehicle
 {
 public:
-    virtual void eat() = 0;
-    virtual void move() = 0;
+	virtual void move() = 0;
+	virtual void crash() = 0;
 };
 
-// Klasa abstrakcyjna
-class Mammal : Animal
+class Bicycle : Vehicle
 {
-public:
-    virtual void drinkMilk()
-    {
-
-    };
-
-    void eat(){}
-    void move(){}
+	public:
+	virtual void change_derailleur()
+	{
+		
+	};
+	void move(){}
+	void crash(){}
 };
-
-// Zwyczajna klasa
-class Human : Mammal
+class Car : Vehicle
 {
-private:
-	char *name;
-	int birthDate;
- 
-public:
-	Human(char *name, int birthDate){};
-	void eat(){};
-	void move(){};
+	public:
+	virtual void drink_fuel()
+	{
+		
+	};
+	void move(){}
+	void crash(){}
 };
-
-class Dog : Mammal
+class Plane : Vehicle
 {
-
+	public:
+	virtual void change_height()
+	{
+	
+	};
+	void move(){}
+	void crash(){}
 };
-
+class PapaMobile : Car
+{
+	private:
+		string owner;
+		char color;
+	public:
+		PapaMobile(string owner, char color){
+			this->owner = owner;
+			this->color = color;
+		};
+		void move(){};
+		void crash(){};
+};	
+class  MountainBike : Bicycle
+{
+	private:
+		string company;
+		int model;
+	public:
+		MountainBike(string company, int model){
+			this->company = company;
+			this->model = model;
+		};
+		void move(){};
+		void crash(){};
+};
+class Tank : Bicycle
+{
+	private:
+		int age;
+		string type;
+	public:
+		Tank(string type, int age){
+			this->type = type;
+			this->age = age;
+		};
+		void move(){};
+		void crash(){};
+};
+class Fighter : Plane
+{
+	private:
+		string country;
+		int value;
+	public:
+		Fighter(string country, int value){
+			this->country = country;
+			this->value = value;
+		};
+		void move(){};
+		void crash(){};
+};
+class RoadBike : Bicycle
+{
+	private:
+		char size;
+		int weight;
+	public:
+		RoadBike(char size, int weight){
+			this->size = size;
+			this->weight = weight;
+		};
+		void move(){};
+		void crash(){};
+};
 int main(int argc, char const *argv[])
- {
- 	Human janek = Human("Janek", 123);
- 	Human anna = Human("Anna", 123);
- 	Human zosia = Human("Zosia", 123);
- 	Dog burek = Dog();
- } 
+{
+    RoadBike janek = RoadBike('m', 4500);
+    Fighter anna = Fighter("Poland", 123);
+    PapaMobile zosia = PapaMobile("JP2", 'w');
+} 
+	
